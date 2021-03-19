c519e7894c5ed190cdf69872926d0a0570321eef
Ferru9 <60952524+Ferru9@users.noreply.github.com>
2021-03-19 17:12:08 -0600

    NumberTable

---
+++ NumberTable.java
@@ -0,0 +1,7 @@
+public class NumberTable {
+    public static void main(String[] args) {
+        Table table1= new Table();
+        table1.getNumbers();
+    }
+    
+}
--- ThePlusBlock.java
+++ ThePlusBlock.java
@@ -4,17 +4,15 @@ public class ThePlusBlock {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int width, height, i, j;
-		
-		
-		System.out.print(" Please enter the height : ");
-		width = sc.nextInt();	
-		
-		System.out.print(" Please enter the width : ");
-		height = sc.nextInt();		
-        
+
+        System.out.print(" Please enter the height : ");
+        width = sc.nextInt();
+
+        System.out.print(" Please enter the width : ");
+        height = sc.nextInt();
+
         for (i = 0; i < width; i++) {
             for (j = 0; j < height; j++) {
-                // center horizontal, center vertical
                 if (i == width / 2 || j == height / 2)
                     System.out.print(" ");
                 else
@@ -22,6 +20,6 @@ public class ThePlusBlock {
             }
             System.out.println();
         }
-        
+
     }
 }
